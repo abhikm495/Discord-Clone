175f9cb84896af0a24730f202f9f49924e442e8d
abhishek <abhikm911@gmail.com>
2024-06-20 23:40:11 +0530

    server-sidebar

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -11,6 +11,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { toast } from "sonner";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
@@ -22,6 +23,7 @@ export function ModeToggle() {
           className="bg-transparent border-0"
           variant="outline"
           size="default"
+          onClick={() => toast.info("Hello")}
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
